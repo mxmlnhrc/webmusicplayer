0c706bc5a77060fc4903a25f24de7beb7b06062a
mxmlnhrc <maxihenri03@gmail.com>
2021-04-04 16:53:41 +0200

    edited name to title in models, also added __str__ methode

--- musicplayer/player/models.py
+++ musicplayer/player/models.py
@@ -4,5 +4,8 @@ from django.db.models.fields import DateTimeCheckMixin, DateTimeField
 
 class Song(models.Model):
     cover = models.ImageField(upload_to='song/cover', blank=True, null=True)
-    name = models.CharField(max_length=255, blank=False, null=False)
+    title = models.CharField(max_length=255, blank=False, null=False)
     feat = models.CharField(max_length=255, blank=True, null=True)
+
+    def __str__(self):
+        return f"Title: {self.title} feat: {self.feat}"
