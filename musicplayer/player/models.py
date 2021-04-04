18d5a63c0630fb473188d1676fb6e8260b07dff0
mxmlnhrc <maxihenri03@gmail.com>
2021-04-04 17:03:55 +0200

    added artist field into song model

--- musicplayer/player/models.py
+++ musicplayer/player/models.py
@@ -5,7 +5,8 @@ from django.db.models.fields import DateTimeCheckMixin, DateTimeField
 class Song(models.Model):
     cover = models.ImageField(upload_to='song/cover', blank=True, null=True)
     title = models.CharField(max_length=255, blank=False, null=False)
+    artist = models.CharField(max_length=255, blank=False, null=False)
     feat = models.CharField(max_length=255, blank=True, null=True)
 
     def __str__(self):
-        return f"Title: {self.title} feat: {self.feat}"
+        return f"Title: {self.title} by: {self.artist}"
