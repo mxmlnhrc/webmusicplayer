371fe04530f7ba9807bbeef4c31f72e0d5a55f2b
mxmlnhrc <maxihenri03@gmail.com>
2021-04-06 11:32:41 +0200

    registered Song, Album and Artist on admin

--- musicplayer/player/admin.py
+++ musicplayer/player/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
-from .models import Song
+from .models import Song, Artist, Album
 
 # Register your models here.
 admin.site.register(Song)
+admin.site.register(Artist)
+admin.site.register(Album)
