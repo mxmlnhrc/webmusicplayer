6978c64d873ebeec33c6b0ffe87827a0ab146bdc
mxmlnhrc <maxihenri03@gmail.com>
2021-04-06 11:46:54 +0200

    added file upload to models

--- musicplayer/player/models.py
+++ musicplayer/player/models.py
@@ -20,8 +20,13 @@ class Album(models.Model):
 class Song(models.Model):
     cover = models.ImageField(upload_to='song/cover', blank=True, null=True)
     title = models.CharField(max_length=255, blank=False, null=False)
+    song = models.FileField(upload_to="song/song")
     artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
     feat = models.CharField(max_length=255, blank=True, null=True)
+    album = models.ForeignKey(Album,
+                              on_delete=models.CASCADE,
+                              blank=True,
+                              null=True)
 
     def __str__(self):
         return f"Title: {self.title} by: {self.artist}"
