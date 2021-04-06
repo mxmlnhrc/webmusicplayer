5210253f4be74e6ef560013af76451cd3ae8897b
mxmlnhrc <maxihenri03@gmail.com>
2021-04-06 11:30:09 +0200

    added Artist and artist fields to the model

--- musicplayer/player/models.py
+++ musicplayer/player/models.py
@@ -5,11 +5,22 @@ from django.db.models.fields import DateTimeCheckMixin, DateTimeField
 class Artist(models.Model):
     name = models.CharField(max_length=255, blank=False)
 
+    def __str__(self):
+        return self.name
+
+
+class Album(models.Model):
+    name = models.CharField(max_length=255, blank=False)
+    artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.name} - {self.artist}"
+
 
 class Song(models.Model):
     cover = models.ImageField(upload_to='song/cover', blank=True, null=True)
     title = models.CharField(max_length=255, blank=False, null=False)
-    artist = models.CharField(max_length=255, blank=False, null=False)
+    artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
     feat = models.CharField(max_length=255, blank=True, null=True)
 
     def __str__(self):
